38019e13393e55f22b039bb83ca1b0340949fc51
Kasper Bjerg <kasperbjerg@kasperbjerg.com>
2024-01-02 22:29:52 +0100

    move sqlite-wasm folder

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,5 @@
-import { default as sqlite3InitModule } from '/vite-react-javascript/node_modules/@sqlite.org/sqlite-wasm/sqlite-wasm/jswasm/sqlite3-bundler-friendly.mjs';
-import '/vite-react-javascript/node_modules/@sqlite.org/sqlite-wasm/sqlite-wasm/jswasm/sqlite3-worker1-promiser-bundler-friendly.js';
+import { default as sqlite3InitModule } from './sqlite-wasm/jswasm/sqlite3-bundler-friendly.mjs';
+import './sqlite-wasm/jswasm/sqlite3-worker1-promiser-bundler-friendly.js';
 
 const sqlite3Worker1Promiser = self.sqlite3Worker1Promiser;
 
