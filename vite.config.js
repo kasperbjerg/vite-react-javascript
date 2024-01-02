b39cae00155eef4f9e835f783b233cb03c1fecd5
Kasper Bjerg <kasperbjerg@kasperbjerg.com>
2024-01-02 22:40:49 +0100

    Try add COOP and COEP as a plugin

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),{
+    enforce: 'pre',
+    name: 'configure-response-headers',
+    configureServer: (server) => {
+      server.middlewares.use((_req, res, next) => {
+        res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+        next();
+      });
+    },
+  }],
   base: "/vite-react-javascript/",
   server: {
     headers: {
