35c29a6c5193ed5af1fab5b96840f92b30416471
Kasper Bjerg <kasperbjerg@kasperbjerg.com>
2024-01-03 22:29:24 +0100

    updating import paths

--- src/Sql.jsx
+++ src/Sql.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
-import sqlite3InitModule from './index.mjs';
+import sqlite3InitModule from '@sqlite.org/sqlite-wasm';
 
 export default function Sql() {
 
