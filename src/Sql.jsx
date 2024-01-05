2d0133f6ea635a8b4eb484fc79d995663d652432
Kasper Bjerg <kasperbjerg@kasperbjerg.com>
2024-01-05 20:10:07 +0100

    Revert "updating import paths"
    This reverts commit 35c29a6c5193ed5af1fab5b96840f92b30416471.

--- src/Sql.jsx
+++ src/Sql.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
-import sqlite3InitModule from '@sqlite.org/sqlite-wasm';
+import sqlite3InitModule from './index.mjs';
 
 export default function Sql() {
 
